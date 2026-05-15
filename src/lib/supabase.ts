f12da1c90bf4b243ec449f8b1f09664359ba2667
pamella <pamellamoraesdeavila@gmail.com>
2026-05-15 10:57:37 -0300

    gitignore

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wyjfilcjcbzjtupdaufc.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5amZpbGNqY2J6anR1cGRhdWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNDIwODcsImV4cCI6MjA5MjkxODA4N30.XRQvRcNRtPF4Kj4iA6oxAOP6sQ6zZarGR3LfnTttWrM'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
